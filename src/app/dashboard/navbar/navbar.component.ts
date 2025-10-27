260369531458d8ed92dd53acc90b9ce9f34617fc
kaungkon.mallick <kaungkon.mallick@datasoft-bd.com>
2025-10-27 09:49:08 +0600

    navbar init

--- src/app/dashboard/navbar/navbar.component.ts
+++ src/app/dashboard/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-navbar',
@@ -7,14 +8,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
- 
+
    name = "Skipper";
    designation = "full stack web-mobile-ml dev";
+  superTasks = [
+    {
+      name: 'KPI Management',
+      subTasks: [
+        { name: 'KPI Dashboard' },
+        { name: 'KPI Configuration' }
+      ]
+    }
+  ];
+    constructor(private router: Router){}
+    ngOnInit() {
 
-   constructor(private router: Router){}
+      // this.superTasks = [
+      //   {
+      //     name: 'KPI Management',
+      //     subTasks: [
+      //       { name: 'KPI Dashboard' },
+      //       { name: 'KPI Configuration' }
+      //     ]
+      //   }
+      // ];
 
-  logout()
-  {
-        this.router.navigate(['']);
+    }
+
+  logout() {
+    this.router.navigate(['']);
   }
 }
