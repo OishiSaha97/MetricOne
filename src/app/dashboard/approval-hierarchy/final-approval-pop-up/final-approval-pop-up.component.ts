86256b520e25739ba7901cff86963c37eeb4e337
kaungkon.mallick <kaungkon.mallick@datasoft-bd.com>
2025-10-29 18:48:28 +0600

    navbar

---
+++ src/app/dashboard/approval-hierarchy/final-approval-pop-up/final-approval-pop-up.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-final-approval-pop-up',
+  templateUrl: './final-approval-pop-up.component.html',
+  styleUrls: ['./final-approval-pop-up.component.css']
+})
+export class FinalApprovalPopUpComponent {
+
+  closePopup() {
+
+  }
+
+  onSubmit() {
+
+  }
+}
