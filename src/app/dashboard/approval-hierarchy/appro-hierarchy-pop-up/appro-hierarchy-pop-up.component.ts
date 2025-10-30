108f54c0094ecc5a4d7a74d76915d1a231807cce
Oishi Saha <oishi.saha@datasoft-bd.com>
2025-10-30 11:59:02 -0700

    commit

--- src/app/dashboard/approval-hierarchy/appro-hierarchy-pop-up/appro-hierarchy-pop-up.component.ts
+++ src/app/dashboard/approval-hierarchy/appro-hierarchy-pop-up/appro-hierarchy-pop-up.component.ts
@@ -21,11 +21,25 @@ export class ApproHierarchyPopUpComponent {
   maxTiers = 4;
   selectedValues: any = {};
   dropdownOpen: { [tier: number]: boolean } = {};
+  userList:any='';
+  filterUserList:any='';
 
-  managers = ['Manager A', 'Manager B', 'Manager C'];
-  approvers = ['Approver X', 'Approver Y', 'Approver Z'];
+  managers = [
+    { id: 1, name: 'Manager A' },
+    { id: 2, name: 'Manager B' },
+    { id: 3, name: 'Manager C' }
+  ];
 
+  approvers = [
+    { id: 1, name: 'Approver X' },
+    { id: 2, name: 'Approver Y' },
+    { id: 3, name: 'Approver Z' }
+  ];
   dropdownOpenStates: { [key: string]: boolean } = {};
+
+  ngOnInit(){
+    this.getUserList();
+  }
 
   addTier() {
     if (this.tiers.length < this.maxTiers) {
@@ -45,17 +59,39 @@ export class ApproHierarchyPopUpComponent {
     this.dropdownOpen[tier] = !this.dropdownOpen[tier];
   }
 
-  selectOption(tier: number, option: string) {
-    this.selectedValues['tier' + tier] = option;
+  selectOption(tier: number, option: { id: number; name: string }) {
+
+    this.selectedValues['tier' + tier] = {
+      index: tier,
+      id: option.id,
+      name: option.name
+    };
+
     this.dropdownOpen[tier] = false;
 
     if (tier === this.tiers[this.tiers.length - 2]) {
       const finalTier = this.tiers[this.tiers.length - 1];
-      this.selectedValues['tier' + finalTier] = option;
+      this.selectedValues['tier' + finalTier] = {
+        index: finalTier,
+        id: option.id,
+        name: option.name
+      };
     }
 
-    console.log("Selected Values:", this.selectedValues);
+    console.log('Selected Values:', this.selectedValues);
   }
+
+  // selectOption(tier: number, option: string) {
+  //   this.selectedValues['tier' + tier] = option;
+  //   this.dropdownOpen[tier] = false;
+  //
+  //   if (tier === this.tiers[this.tiers.length - 2]) {
+  //     const finalTier = this.tiers[this.tiers.length - 1];
+  //     this.selectedValues['tier' + finalTier] = option;
+  //   }
+  //
+  //   console.log("Selected Values:", this.selectedValues);
+  // }
 
   isDropdownOpen(tier: number) {
     return this.dropdownOpen[tier];
@@ -70,12 +106,14 @@ export class ApproHierarchyPopUpComponent {
   }
 
   onSubmit() {
-    let obj ={
-      tier:this.selectedValues,
-      approver:this.finalApprover,
-      team:this.teamName,
-      param:'kpi_insert_hierarchy_data'
-    }
+    const tierArray = Object.values(this.selectedValues);
+
+    let obj = {
+      tier: JSON.stringify(tierArray),
+      approver: this.finalApprover,
+      team: this.team_name,
+      param: 'kpi_insert_hierarchy_data'
+    };
     console.log('Submitting Objectives:', obj);
     this.kpi.saveKpiHierarchy(obj).subscribe({
       next: (response) => {
@@ -85,6 +123,19 @@ export class ApproHierarchyPopUpComponent {
 
       }
     });
+  }
+
+
+  getUserList() {
+    this.kpi.getLogData({params: 'user-name-list'})
+      .subscribe(res => {
+         this.userList = res?.['user-name-list'];
+         this.filterUserList = res?.['user-name-list'];
+        }, error => {
+          // this.alerts.closeAlert();
+          // this.alerts.toast('error', 'Unable to fetch incident Category List.  Please try again. If the problem persists then please contact our Support Team')
+        }
+      );
   }
 
 
