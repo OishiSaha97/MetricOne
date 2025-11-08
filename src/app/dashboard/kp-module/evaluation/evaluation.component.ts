9a726cd87a6b4a6b9b43aadbfce412c57c297023
Raisul Kabir <raisul.kabir@datasoft-bd.com>
2025-11-08 16:12:49 +0600

    evaluation

--- src/app/dashboard/kp-module/evaluation/evaluation.component.ts
+++ src/app/dashboard/kp-module/evaluation/evaluation.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./evaluation.component.css']
 })
 export class EvaluationComponent {
+  tables: { [key: string]: { selected: boolean } } = {
+    values: { selected: true },
+    manager: { selected: false },
+  };
+  currentTable: string = 'values';
 
+  ngOnInit(){
+    this.currentTable = 'values';
+  }
+  changeTable(tab: string) {
+    this.tables[this.currentTable].selected = false;
+
+
+    this.currentTable = tab;
+
+    this.tables[this.currentTable].selected = true;
+  }
 }
--- src/app/dashboard/kp-module/evaluation/values-component/values-component.component.ts
+++ src/app/dashboard/kp-module/evaluation/values-component/values-component.component.ts
@@ -1,11 +1,66 @@
 import { Component } from '@angular/core';
-
+interface Objective {
+  id: number;
+  title: string;
+  selectedRating: string;
+  objectiveText: string;
+  targetText: string;
+  isOpen: boolean;
+  keyObjective?: string;
+  keyTarget?: string;
+}
 @Component({
   selector: 'app-values-component',
   templateUrl: './values-component.component.html',
   styleUrls: ['./values-component.component.css']
 })
 export class ValuesComponentComponent {
-  objectives: any = [];
+  objectives: any = [{name:'DEPENDABILITY',isOpen: false, isEditingObjective:false},
+    {name:'JOB KNOWLEDGE AND SKILLS',isOpen: false, isEditingObjective:false}
+    , {name:'INITIATIVE AND RESOURCEFULNESS',isOpen: false, isEditingObjective:false},
+    {name:'JUDGEMENT',isOpen: false, isEditingObjective:false},
+    {name:'ADAPTABILITY',isOpen: false, isEditingObjective:false},
+    {name:'DECISIVENESS',isOpen: false, isEditingObjective:false},
+    {name: 'INTERPERSONAL RELATIONSHIPS', isOpen: false, isEditingObjective:false}];
+  isOpen: boolean[] = [];
+  mode:any
+  objectiveTypes: string[] = ['Production', 'Support', 'Innovation', 'People', 'Other'];
+  rating: any[] = ['Role Model', 'Very Good', 'Good', 'Improvement Required', 'Unacceptable'];
+  overAllRating: any;
 
+  addObjective(): void {
+    const newObjective: Objective = {
+      id: this.objectives.length + 1,
+      title: `Work Objective ${this.objectives.length + 1}`,
+      selectedRating: '',
+      objectiveText: '',
+      targetText: '',
+      isOpen: false
+    };
+    this.objectives.push(newObjective);
+  }
+
+  onObjectiveChange(type: any, obj: Objective,i:number): void {
+    obj.selectedRating = type;
+    this.isOpen[i] = false;
+    console.log(`Objective ${obj.id} selected rating:`, obj.selectedRating);
+  }
+  toggleObjective(obj: Objective): void {
+    obj.isOpen = !obj.isOpen;
+  }
+  toggleObjectiveEdit(obj: any) {
+    obj.isEditingObjective = !obj.isEditingObjective;
+  }
+
+  onBack() {
+
+  }
+
+  onNext() {
+
+  }
+
+  onOverallRating(type: any) {
+    this.overAllRating = type;
+  }
 }
