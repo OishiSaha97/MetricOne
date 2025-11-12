802e391397fd02c51df2c5a90a6d7ccf784c89dd
kaungkon.mallick <kaungkon.mallick@datasoft-bd.com>
2025-11-12 18:00:38 +0600

    setting

--- src/app/dashboard/navbar/navbar.component.ts
+++ src/app/dashboard/navbar/navbar.component.ts
@@ -139,6 +139,9 @@ export class NavbarComponent {
         if(this.initialtionDate && (kpiDate > today)){
           localStorage.setItem('timePeriod', "evaluation");
         }
+        else if(this.initialtionDate === ''){
+          localStorage.setItem('timePeriod', "new year");
+        }
 
       });
   }
