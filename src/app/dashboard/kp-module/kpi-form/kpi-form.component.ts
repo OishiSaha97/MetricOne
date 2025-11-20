cea130ea5e9f4f1c3f100b6c7a64824a44064087
Oishi Saha <oishi.saha@datasoft-bd.com>
2025-11-20 12:39:54 -0800

    modification

--- src/app/dashboard/kp-module/kpi-form/kpi-form.component.ts
+++ src/app/dashboard/kp-module/kpi-form/kpi-form.component.ts
@@ -98,7 +98,7 @@ export class KpiFormComponent implements OnInit {
       if(this.approvalStatus == 'Reverted' ){
         this.getData();
       }
-      if(this.mode == 'approver' || this.view == false){
+      if(this.mode == 'approver' || this.view == false || this.view == true){
         this.kpi.getLogData({userIdKPI:this.userId,param: 'kpi-list',objectId:this.kpiUserId,parameter:this.team,pid:this.year})
           .subscribe(res => {
               this.data = Array.isArray(res?.['kpi-list']) ? res?.['kpi-list'] : res?.['kpi-list']
