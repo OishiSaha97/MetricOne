488530a27c2d71c49b2038521b97f0e81323b09b
Oishi Saha <oishi.saha@datasoft-bd.com>
2025-11-24 18:27:21 -0800

    modification

--- src/app/dashboard/kp-module/evaluation/objective-set/objective-set.component.ts
+++ src/app/dashboard/kp-module/evaluation/objective-set/objective-set.component.ts
@@ -406,6 +406,12 @@ export class ObjectiveSetComponent {
       this.showToast(`Total weightage must be exactly 100%. Current total: ${totalWeightage}%`);
       return false;
     }
+    const hasErrors = Object.values(this.objectiveErrors).some(err => Object.keys(err).length > 0);
+    if (hasErrors) {
+
+      this.showToast("Please fill all required fields before submitting!");
+      return false;
+    }
 
     return !hasError;
   }
@@ -520,9 +526,9 @@ export class ObjectiveSetComponent {
     console.log(obj)
     this.kpi.getLogData({userIdKPI:this.userId,param: 'changed-target-history',objectId:this.kpiUserId,parameter:this.team,pid:this.year,extraParam:this.kpiId})
       .subscribe(res => {
-
           this.changedTargetHistory = Array.isArray(res?.['changed-target-history']) ? res?.['changed-target-history'] : res?.['changed-target-history']
           console.log(this.changedTargetHistory);
+          this.openChangedTargetHistory();
         },
         (error) => {
           console.error("Error fetching permission list", error);
@@ -531,7 +537,7 @@ export class ObjectiveSetComponent {
   }
 
   openObjectiveHistory(obj: any,i:any) {
-    this.showObjectiveHistoryIndex = null;
+    //this.showObjectiveHistoryIndex = null;
     this.kpi.getLogData({param: 'changed-objective-history',objectId:obj.id,parameter:this.team,pid:this.year,extraParam:obj.workId})
       .subscribe(res => {
           this.changedObjHistory = Array.isArray(res?.['changed-objective-history']) ? res?.['changed-objective-history'] : res?.['changed-objective-history']
@@ -543,6 +549,9 @@ export class ObjectiveSetComponent {
       );
   }
 
+  openChangedTargetHistory() {
+    this.showTargetHistory = !this.showTargetHistory;
+  }
 
   // getAttribute() {
   //   this.kpi.getLogData({param: 'attributeType'})
@@ -580,21 +589,31 @@ export class ObjectiveSetComponent {
     this.showHistory = !this.showHistory;
   }
 
-  openChangedTargetHistory() {
-
-  }
-
   openChangedObjectiveHistory(i: number) {
 
+      if (this.showObjectiveHistoryIndex === i) {
+      this.showObjectiveHistoryIndex = null;
+    } else {
+      this.showObjectiveHistoryIndex = i;
+    }
+
   }
 
 
-  openAchievedHistory(obj: any) {
-
+  openAchievedHistory() {
+    this.kpi.getLogData({userIdKPI:this.userId,param: 'changed-achieved-history',objectId:this.kpiUserId,parameter:this.team,pid:this.year,extraParam:this.kpiId})
+      .subscribe(res => {
+          this.changedPerformanceHistory = Array.isArray(res?.['changed-achieved-history']) ? res?.['changed-achieved-history'] : res?.['changed-achieved-history']
+          this.openChangedAchievedHistory();
+        },
+        (error) => {
+          console.error("Error fetching permission list", error);
+        }
+      );
   }
 
   openChangedAchievedHistory() {
-
+    this.showAchievedHistory = !this.showAchievedHistory;
   }
 
   toggleAchievedEdit(obj: any) {
