782bf7834886448c7c714473d454d7fbe364e6a7
kaungkon <mallickkaungkon@gmail.com>
2025-11-27 10:00:07 +0600

    setting

--- src/app/dashboard/list/list.component.ts
+++ src/app/dashboard/list/list.component.ts
@@ -235,6 +235,37 @@ export class ListComponent {
           dataLoader.unsubscribe();
         });
       }
+
+      else{
+        let view = false;
+        if((user.status ==="Published" ||user.status ==="Closed")  && !user.edit_permission){
+          view = true;
+        }
+        const initialState = {
+          kpiUserId: user.user_id,
+          status: user.status,
+          team: user.team,
+          name: user.name,
+          year: user.year,
+          kpiId: user.id,
+          approvalStatus: user.approval_status,
+          currentStatus:'employee',
+          view:view,
+          draftShow:true
+        };
+        this.modalRef = this.modalService.show(KpiFormComponent, {
+          backdrop: 'static',
+          keyboard: false,
+          class: 'modal-dialog modal-dialog-centered modal-max',
+          initialState: initialState
+        });
+
+        let dataLoader = this.modalRef.content.saveEmitter.subscribe((res:any) => {
+          this.loadData({});
+          dataLoader.unsubscribe();
+        });
+
+      }
     }
 
   // activeDropdown: HTMLElement | null = null;
