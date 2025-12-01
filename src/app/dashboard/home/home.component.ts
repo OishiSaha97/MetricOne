e4ec5da096d3560414e23bff21b4e0adefbdad35
kaungkon <mallickkaungkon@gmail.com>
2025-12-01 13:33:49 +0600

    setting

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -131,9 +131,10 @@ export class HomeComponent {
         else{
           this.settingTitle = "SETTING";
           this.mode = "add";
+          this.checkEvaEndDate();
         }
 
-        this.checkEvaEndDate();
+
       });
   }
   formatDateForInput(dateString: string): string {
